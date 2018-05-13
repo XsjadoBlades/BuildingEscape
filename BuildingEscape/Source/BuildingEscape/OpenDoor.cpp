99ad89206030213b4f73f29194c59bba5ee82d6c
Jared Luke Morgan <jared@analytical-logic.com>
2018-05-13 14:47:12 +0200

    BE37 "Exposing Events to Blueprint" Section 3, Lecture 97

--- BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -34,7 +34,8 @@ void UOpenDoor::OpenDoor()
 {
 	//Rotate the door to an open state
 	if (!Owner) { return; }
-	Owner->SetActorRotation(FRotator(0.0f, OpenAngle, 0.0f));
+	//Owner->SetActorRotation(FRotator(0.0f, OpenAngle, 0.0f));
+	OnOpenRequest.Broadcast();
 }
 
 void UOpenDoor::CloseDoor()
